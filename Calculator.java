657f30d3e5c1d734a5b41e060e282f5a23b8c565
Selasi-Afi <selasigborglah@gmail.com>
2017-01-21 21:44:59 +0000

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+public class Calc{
+	public static void main String [] args {
+
+		if ( args[0].equals("add")){
+			double firstNumber= Double.parseDouble(args[1]);
+			double secondNumber= Double.parseDouble(args[2]);
+
+			System.out.println(firstNumber+secondNumber);
+		}
+
+
+
+
+	else if ( args[1].equals("subtract")){
+			double firstNumber= Double.parseDouble(args[1]);
+			double secondNumber= Double.parseDouble(args[2]);
+
+			System.out.println(firstNumber-secondNumber);
+		}	
+	
+		else if ( args[1].equals("multiply")){
+			double firstNumber= Double.parseDouble(args[1]);
+			double secondNumber= Double.parseDouble(args[2]);
+
+			System.out.println(firstNumber*secondNumber);
+		}
+	}
+}
+
